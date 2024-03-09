1b20e5b58a9abb8771e87261b0e303d9517903d6
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-03-09 23:00:35 +0530

    Time: 63 ms (86.75%) | Memory: 52.9 MB (73.96%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -4,7 +4,7 @@ public:
         int i=0,j=0;
         while(i<nums1.size() && j<nums2.size()){
             if(nums1[i]==nums2[j]) return nums1[i];
-            if(nums1[i]>nums2[j]) j+=1;
+            else if(nums1[i]>nums2[j]) j+=1;
             else i+=1;
         }
         return -1;
