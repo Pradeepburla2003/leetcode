8ae91444cd4caeede3a1685e15313cbbcd1b55f8
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-03-15 18:11:27 +0530

    Time: 12 ms (83.78%) | Memory: 26.3 MB (72.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -6,13 +6,13 @@ public:
             if(nums[i]!=0) sum=sum*nums[i];
             else c+=1;
         }
-        vector<int>ans;
+        // vector<int>ans;
         for(int i=0;i<nums.size();i++){
-            if(c>1) ans.push_back(0);
-            else if(nums[i]==0) ans.push_back(sum);
-            else if(c>0) ans.push_back(0);
-            else ans.push_back(sum/nums[i]);
+            if(c>1) nums[i]=(0);
+            else if(nums[i]==0) nums[i]=sum;
+            else if(c>0) nums[i]=0;
+            else nums[i]=sum/nums[i];
         }
-        return ans;
+        return nums;
     }
 };
