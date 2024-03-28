5c6eec74bff991b4e5471eebfdc6fbf795373afe
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-03-28 23:09:01 +0530

    Time: 196 ms (87.55%) | Memory: 148.4 MB (57.21%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
-        map<int,int>mp;
+        unordered_map<int,int>mp;
         int maxi=1,j=0,i=0,n=nums.size();
         while(j<n){
             mp[nums[j]]++;
