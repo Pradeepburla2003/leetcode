dafd204b27d63025e3f0808dcd7815f672f4fffb
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-06-08 18:33:09 +0530

    Time: 8 ms (14.58%) | Memory: 14.6 MB (14.72%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    set<vector<int>>s;
+    void permutation(vector<int>&nums,vector<int>&visited,vector<int>&temp){
+        for(int j=0;j<nums.size();j++){
+            if(visited[j]==0){
+                visited[j]=1;
+                temp.push_back(nums[j]);
+                permutation(nums,visited,temp);
+                if(temp.size()==nums.size()) s.insert(temp);
+                temp.pop_back();
+                visited[j]=0;
+            }
+        }
+    }
+    void nextPermutation(vector<int>& nums) {
+        // if(nums.size()==1) return;
+        // for(int i=0;i<nums.size();i++){
+        //     vector<int>visted(nums.size(),0);
+        //     vector<int>temp;
+        //     temp.push_back(nums[i]);
+        //     visted[i]=1;
+        //     permutation(nums,visted,temp);
+        // }
+        // vector<vector<int>>myvec(s.begin(),s.end());
+        // auto i=s.find(nums);
+        // int pos=distance(s.begin(),i);
+        // pos+=1;
+        // pos=pos%(myvec.size());
+        // for(int j=0;j<nums.size();j++){
+        //     nums[j]=myvec[pos][j];
+        // 
+        int a[nums.size()];
+        int n=nums.size();
+        for(int i=0;i<nums.size();i++){
+            a[i]=nums[i];
+        }
+        next_permutation(a,a+n);
+        for(int i=0;i<n;i++){
+            nums[i]=a[i];
+        }
+    }
+};
