8b51cfd6f96bbad26efee44da7c8ce017082681e
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-06-24 14:33:26 +0530

    Time: 22 ms (5.08%) | Memory: 8.4 MB (96.75%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return 0;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]!=t[i]) return 0;
+        // }
+        if(s!=t) return 0;
+        return 1;
+    }
+};
