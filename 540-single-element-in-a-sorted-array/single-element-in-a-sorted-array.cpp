1c15a5e0e8667bf15701fff37b0d19cdf1e93748
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-07-02 19:27:09 +0530

    Time: 15 ms (84.83%) | Memory: 24.8 MB (13.99%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            ans=ans^nums[i];
+        int n=nums.size();
+        
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        int start=1,end=n-2;
+        while(start<=end){
+            int mid=(start+end)/2;
+            cout<<mid<<" "<<nums[mid]<<endl;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+            if((mid%2 and nums[mid]==nums[mid-1]) or (mid%2==0 and nums[mid]==nums[mid+1])){
+                start=mid+1;
+            }
+            else end=mid-1;
         }
-        return ans;
+        return 1;
     }
 };
