091d71ee5797985a1efca76ee4cdbae99a62936d
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-09-26 19:20:03 +0530

    Time: 3 ms (53.75%) | Memory: 10.6 MB (41.24%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int fun(int i,int j,int m,int n,vector<vector<int>>& obstacleGrid,vector<vector<int>>& dp){
-        if(i==m-1 and j==n-1) return 1;
-        if(i>=m or j>=n or obstacleGrid[i][j]==1) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int bottom=fun(i+1,j,m,n,obstacleGrid,dp);
-        int right=fun(i,j+1,m,n,obstacleGrid,dp);
-        // cout<<bottom<<" "<<right<<endl;
-        return dp[i][j]=bottom+right;
+    int help(vector<vector<int>>& obstacleGrid,int i,int j,int m,int n,vector<vector<int>>& dp){
+        if(i == m-1 && j == n-1) return 1;
+        if(i >= m || j >= n || obstacleGrid[i][j]) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int right = help(obstacleGrid,i,j+1,m,n,dp);
+        int down = help(obstacleGrid,i+1,j,m,n,dp);
+        // cout<<right<<" "<<down<<endl;
+        return dp[i][j] = right + down;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m=obstacleGrid.size(),n=obstacleGrid[0].size();
-        if(obstacleGrid[m-1][n-1]==1) return 0;
+        int m = obstacleGrid.size(),n = obstacleGrid[0].size();
+        if(obstacleGrid[m-1][n-1] == 1) return 0;
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return fun(0,0,m,n,obstacleGrid,dp);
-
+        return help(obstacleGrid,0,0,m,n,dp);
     }
 };
