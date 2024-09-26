3fbc8b7b9fce2348fa5b03e421437624c845e796
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-09-26 17:20:58 +0530

    Time: 46 ms (34.74%) | Memory: 84.4 MB (5.14%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int fun(vector<int>&nums,int amount,int i,vector<vector<int>>&dp){
-
-        if(amount==0) return amount;
-        if(amount<0 or i==nums.size()) return  100000;
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        int pick=1+fun(nums,amount-nums[i],i,dp);
-        int nonpick=fun(nums,amount,i+1,dp);
-        // cout<<pick<<" "<<nonpick<<endl;
-        return dp[i][amount]=min(pick,nonpick);
+    int help(vector<int>& coins,int i, int amount,vector<vector<int>>&dp){
+        if(amount == 0) return 0;
+        if(i >= coins.size() || amount < 0) return 1e8;
+        if(dp[i][amount] != -1) return dp[i][amount];
+        int pick = INT_MAX;
+        if(coins[i] <= amount) pick = 1 + help(coins,i,amount - coins[i],dp);
+        int nonpick = help(coins,i+1,amount,dp);
+        return dp[i][amount] = min(pick,nonpick);
     }
     int coinChange(vector<int>& coins, int amount) {
+        if(amount == 0) return 0;
         vector<vector<int>>dp(coins.size(),vector<int>(10002,-1));
-        int ans=fun(coins,amount,0,dp);
-        if(ans>=100000) return -1;
+        int ans = help(coins,0,amount,dp);
+        if(ans >= 1e8) return -1;
         return ans;
     }
 };
