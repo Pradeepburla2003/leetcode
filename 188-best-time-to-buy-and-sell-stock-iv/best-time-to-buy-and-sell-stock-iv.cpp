415a1cb6a1b16b782c8ad09f5cf3a3fa2d4c5c42
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-09-28 21:02:48 +0530

    Time: 16 ms (34.15%) | Memory: 17 MB (28.90%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,12 +3,14 @@ public:
     int help(vector<int>& prices,int buy,int ind,int cap,int n,vector<vector<vector<int>>>& dp){
         if(ind == n or cap == 0) return 0;
         if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap]; 
+        int profit = 0;
         if(buy){
-            return dp[ind][buy][cap] = max(-prices[ind] + help(prices,0,ind+1,cap,n,dp),0 + help(prices,1,ind+1,cap,n,dp));
+            profit = max(-prices[ind] + help(prices,0,ind+1,cap,n,dp),0 + help(prices,1,ind+1,cap,n,dp));
         }
         else{
-            return dp[ind][buy][cap] = max(prices[ind] + help(prices,1,ind+1,cap-1,n,dp),0 + help(prices,0,ind+1,cap,n,dp));
+            profit = max(prices[ind] + help(prices,1,ind+1,cap-1,n,dp),0 + help(prices,0,ind+1,cap,n,dp));
         }
+        return dp[ind][buy][cap] = profit;
     }
     int maxProfit(int k,vector<int>& prices) {
         int n = prices.size();
