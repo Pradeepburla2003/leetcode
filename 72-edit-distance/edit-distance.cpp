6c3748e9a912137585a6b6217f2ad9fa6f33dbf5
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-09-29 10:44:22 +0530

    Time: 239 ms (5.02%) | Memory: 403 MB (5.11%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int fun(string word1, string word2,int i,int j,vector<vector<int>>&dp){
-        if(j==word2.size()) return word1.size()-i;
-        if(i==word1.size()) return word2.size()-j;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(word1[i]==word2[j]){
-            return fun(word1,word2,i+1,j+1,dp);
+    int help(string word1, string word2,int i,int j,int m,int n,vector<vector<int>>&dp){
+        if(i == m) return n - j;
+        if(j == n) return m - i;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(word1[i] == word2[j]){
+            return help(word1,word2,i+1,j+1,m,n,dp);
         }
         else{
-            int in=1+fun(word1,word2,i,j+1,dp);
-            int de=1+fun(word1,word2,i+1,j,dp);
-            int re=1+fun(word1,word2,i+1,j+1,dp);
-            return dp[i][j]=min(in,min(de,re));
+            int insert = 1 + help(word1,word2,i,j+1,m,n,dp);
+            int remove = 1 + help(word1,word2,i+1,j,m,n,dp);
+            int replace = 1 + help(word1,word2,i+1,j+1,m,n,dp);
+            return dp[i][j] = min(insert,min(replace,remove));
         }
-    }     
+    }
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),-1));
-        return fun(word1,word2,0,0,dp);
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return help(word1,word2,0,0,m,n,dp);
     }
 };
