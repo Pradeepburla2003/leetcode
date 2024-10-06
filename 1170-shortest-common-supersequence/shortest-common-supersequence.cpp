78346fbd9eaf9f48f58c5c6bcc65bba6526da2b6
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-10-06 09:59:47 +0530

    Time: 29 ms (17.93%) | Memory: 25.9 MB (34.02%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -3,11 +3,7 @@ public:
     string shortestCommonSupersequence(string str1, string str2) {
         int m=str1.size();
         int n=str2.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        for(int i=0;i<m+1;i++){
-            dp[i][0]=0;
-        }
-        for(int i=0;i<=n;i++) dp[0][i]=0;
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(str1[i-1]==str2[j-1]){
