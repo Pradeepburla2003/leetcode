5c78b71c1aeef36ce9db1e648b11ea9be4a100d3
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-10-08 22:58:41 +0530

    Time: 10 ms (66.86%) | Memory: 19.1 MB (91.83%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int help(vector<int>& nums,int i,vector<int>& dp){
-        if(i == nums.size() - 1) return 0;
-        if(i >= nums.size()) return 1e8;
-        if(dp[i] != -1) return dp[i];
-        int count = 1e8;
-        for(int j=1; j<=nums[i]; j++){
-            count = min(count,1 + help(nums,i+j,dp));
-        }
-        return  dp[i] = count;
-    }
     int jump(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        return help(nums,0,dp);
+
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            farthest = max(farthest, i + nums[i]);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+                if (currentEnd >= nums.size() - 1) break;
+            }
+        }
+        return jumps;
     }
 };
