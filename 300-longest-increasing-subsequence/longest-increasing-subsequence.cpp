2ca530ed022c209867c54c5ec6d3b14ac0afa766
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-10-11 08:06:08 +0530

    Time: 222 ms (36.46%) | Memory: 294.9 MB (21.18%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
-    int help(vector<int>&nums,int j,int i,vector<vector<int>>&dp){
+    int help(vector<int>&nums,int i,int j,vector<vector<int>>&dp){
         if(i >= nums.size()) return 0;
         if(dp[i][j+1] != -1) return dp[i][j+1];
         int pick = 0;
-        if(j==-1 || nums[i] > nums[j]) pick = 1 + help(nums,i,i+1,dp);
-        int nonpick = help(nums,j,i+1,dp);
+        if(j==-1 || nums[i] > nums[j]) pick = 1 + help(nums,i+1,i,dp);
+        int nonpick = help(nums,i+1,j,dp);
         return dp[i][j+1] = max(pick,nonpick);
     }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size(),-1));
-        return help(nums,-1,0,dp);
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,0));
+        int res = help(nums,0,-1,dp);
+        int n = nums.size();
+        // return res;
+        for(int i=n-1;i>=0;i--){
+            for(int j=i-1;j>=-1;j--){
+                int pick = 0;
+                if(j==-1 || nums[i] > nums[j]) pick = 1 + dp[i+1][i+1];
+                int nonpick = dp[i+1][j+1];
+                dp[i][j+1] = max(pick,nonpick);
+            }
+        }
+        return dp[0][0];
     }
 };
