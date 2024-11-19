15fe33b096e8d97bbaefc0369f2c9f976e01fbfb
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2024-11-19 11:01:23 +0530

    Time: 326 ms (35.09%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,11 +1,2 @@
-SELECT 
-    CASE 
-        WHEN COUNT(distinct salary) < 2 THEN NULL
-        ELSE (
-            SELECT distinct salary
-            FROM employee
-            ORDER BY salary DESC
-            LIMIT 1,1
-        )
-    END AS SecondHighestSalary
-FROM employee;
+SELECT MAX(salary) as SecondHighestSalary from Employee
+ where salary!=(SELECT MAX(salary) from Employee);
