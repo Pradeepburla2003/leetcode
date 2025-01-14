79469ed96e7160832257a93623e97619609662b5
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2025-01-14 14:18:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (44.05%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    void perm(vector<int>& nums,int n,vector<int>&v,vector<int>&temp, vector<vector<int>>&ans){
-        if(temp.size()==n){
+    void help(vector<int>& nums,vector<int>& temp,vector<vector<int>>&ans,vector<int>&vis,int i){
+        // cout<<temp.size()<<endl;
+        if(temp.size()==nums.size()){
             ans.push_back(temp);
-            return ;
+            return;
         }
-        for(int i=0;i<n;i++){
-            if(v[i]==0){
-                v[i]=1;
-                temp.push_back(nums[i]);
-                perm(nums,n,v,temp,ans);
-                v[i]=0;
+        for(int j=0;j<nums.size();j++){
+            if(vis[j]==0){
+                temp.push_back(nums[j]);
+                vis[j]=1;
+                help(nums,temp,ans,vis,j);
+                vis[j]=0;
                 temp.pop_back();
             }
         }
+        
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>temp,v(n,0);
         vector<vector<int>>ans;
-        perm(nums,n,v,temp,ans);
+        vector<int>temp;
+        for(int i=0;i<nums.size();i++){
+            temp.push_back(nums[i]);
+            vector<int>vis(nums.size(),0);
+            vis[i]=1;
+            help(nums,temp,ans,vis,i);
+            temp.pop_back();
+        }
         return ans;
     }
-};  
+};
