3b038fadce04d1288a8cf8a737558da117642266
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2025-01-14 15:25:33 +0530

    Time: 6 ms (57.40%) | Memory: 13.1 MB (57.74%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,22 +1,25 @@
-class Solution{
+class Solution {
 public:
-    bool solve(int ind,string &s,int str_len,unordered_map<string,int> &mp,vector<int> &dp){
-        if(ind==str_len) return true;
-        if(dp[ind]!=-1) return dp[ind];
-        for(int i=ind;i<str_len;i++){
-            if(mp.find(s.substr(ind,i-ind+1))!=mp.end()){
-                if(solve(i+1,s,str_len,mp,dp))  return dp[ind]=true;
+    bool help(string &s,int i,map<string,int>&mp,string temp,vector<int>&dp){
+        // cout<<temp<<" ";
+        if(s.size()==i) return (s==temp);
+        if(dp[i]!=-1) return dp[i];
+        string word="";
+        // cout<<word<<" "
+        for(int j=i;j<s.size();j++){
+            word+=s[j];
+            if(mp.find(word)!=mp.end()){
+                bool found = help(s,j+1,mp,temp+word,dp);
+                if(found) return dp[i]=true;
             }
         }
-        return dp[ind]=false;
+        return dp[i]=false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        int size=wordDict.size();
-        unordered_map<string,int> mp;
-        for(int i=0;i<size;i++)
-            mp[wordDict[i]]=1;
-        int str_len=s.size();
-        vector<int> dp(str_len+1,-1);
-        return solve(0,s,str_len,mp,dp);
+        map<string,int>mp;
+        for(int i=0;i<wordDict.size();i++) mp[wordDict[i]]++;
+        string temp="",word="";
+        vector<int>dp(s.size(),-1);
+        return help(s,0,mp,temp,dp);
     }
 };
