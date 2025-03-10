fff4aff7e5e12324b4c706848a221cf411b5bb55
Pradeep Kumar <97887582+Pradeepburla2003@users.noreply.github.com>
2025-03-10 12:54:11 +0530

    Time: 0 ms (100.00%) | Memory: 25.5 MB (91.44%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        vector<int>nums((grid.size()*grid.size())+1,0);
+        int xorVal=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid.size();j++){
-                nums[grid[i][j]]++;
+                xorVal^=grid[i][j];
             }
         }
-        int repeat,missing;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==2) repeat=i;
-            if(nums[i]==0) missing=i;
+        int n=grid.size()*grid.size(),i=1;
+        while(i<=n) xorVal^=(i++);
+        int bitPos=0;
+        while(bitPos<32){
+            if(xorVal&(1<<bitPos)) break;
+            bitPos++;
         }
-        return {repeat,missing};
+        int zeros=0,ones=0;
+        for(i=0;i<grid.size();i++){
+            for(int j=0;j<grid.size();j++){
+                if(grid[i][j]&(1<<bitPos)) ones^=grid[i][j];
+                else zeros^=grid[i][j];
+            }
+        }
+        for(i=1;i<=n;i++){
+            if(i&(1<<bitPos)) ones^=i;
+            else zeros^=i;
+        }
+        int zeroCount=0,oneCount=0;
+        for(i=0;i<grid.size();i++){
+            for(int j=0;j<grid.size();j++){
+                if(zeros==grid[i][j]) zeroCount++;
+            }
+        }
+        // cout<<zeros<<" "<<zeroCount<<endl<<ones<<" "<<oneCount<<endl;
+        if(zeroCount==2) return {zeros,ones};
+        return {ones,zeros};
     }
 };
